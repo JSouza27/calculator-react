e279d5036a3b4841ff34daf508837bfaf257c2d5
Jonathan Souza <souzajonathan570@gmail.com>
2022-06-03 11:18:24 -0300

    feat: config display

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Display from './components/Display';
 import Keyboard from './components/Keyboard';
 import GlobalStyle from './css/GlobalStyle';
 import { AppWrapper, AppContainer } from './Style';
@@ -6,6 +7,7 @@ import { AppWrapper, AppContainer } from './Style';
 const App = () => (
   <AppWrapper className="App">
     <AppContainer>
+      <Display />
       <Keyboard />
     </AppContainer>
     <GlobalStyle />
